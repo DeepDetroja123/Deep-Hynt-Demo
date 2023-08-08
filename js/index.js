24f82c14a2ab66cb9917b52580b0ef7a71cf96f9
DeepDetroja123 <deep@praella.com>
2023-08-08 18:59:35 +0530

    set search view on click and continue

--- js/index.js
+++ js/index.js
@@ -2,25 +2,31 @@ function toggleNav() {
     var before_class = document.getElementById("menu-icon-mobile");
     var after_class = document.getElementById("cancel-icon");
     var hidden_class = document.getElementById("mobileNav");
+    var hidden_ele_class = document.getElementById("search-line");
+
     if (before_class.classList.contains('d-block')) {
         before_class.classList.remove('d-block')
         before_class.classList.add('d-none');
+      
     }
     else {
         before_class.classList.remove('d-none');
         before_class.classList.add('d-block');
+
     }
     if (after_class.classList.contains('d-none')) {
         after_class.classList.remove('d-none');
         after_class.classList.add('d-block');
-        hidden_class.classList.remove('d-none');
-        hidden_class.classList.add('d-block');
+        // hidden_ele_class.classList.remove('d-none');
+        // hidden_ele_class.classList.add('d-block');
+        hidden_class.style.left="0";
     }
     else {
         after_class.classList.remove('d-block');
         after_class.classList.add('d-none');
-       hidden_class.classList.remove('d-block');
-       hidden_class.classList.add('d-none');
+        // hidden_class.classList.remove('d-block');
+        // hidden_class.classList.add('d-none');
+        hidden_class.style.left="-100%";
     }
 }
 
